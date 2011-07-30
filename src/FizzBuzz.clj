17bae483f8fe6857cd4dfbefa83d185f604e1324
Stefan Bley <stefan.bley@saxsys.de>
2011-07-30 22:37:57 +0200

    code formatting

--- src/FizzBuzz.clj
+++ src/FizzBuzz.clj
@@ -3,5 +3,11 @@
   "Returns the input number, but for multiples of 3 it returns 'fizz' 
   and for multiples of 5 it returns 'buzz'."
   [number]
-  (if (= 0 (rem number 3)) 'fizz (if (= 0 (rem number 5)) 'buzz number))
+  (if (= 0 (rem number 3)) 
+    'fizz 
+    (if (= 0 (rem number 5)) 
+      'buzz 
+      number
+    )
+  )
 )
